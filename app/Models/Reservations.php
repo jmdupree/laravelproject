1d1312f5fc59005ff65bae6d713ee29f6a4b730f
jmdupree <jmdupree#@g.coastal.edu>
2020-02-16 22:11:00 -0500

    Initial commit

---
+++ app/Models/Reservations.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Reservations extends Model
+{
+       protected $fillable = [
+ 'user_id',
+ 'room_id',
+ 'num_of_guests',
+ 'arrival'
+ 'departure'
+ ];
+ public function rooms() {
+ return $this->belongsTo('App\Models\Room');
+ public function user() {
+ return $this->belongsTo('App\User');
+
+}
