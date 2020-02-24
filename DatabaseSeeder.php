9e6aff58c82a1eeb239b0849a02d084776368960
jmdupree <59934707+jmdupree@users.noreply.github.com>
2020-02-23 22:24:59 -0500

    Loaded Seed Data

---
+++ DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+
+	UsersTableSeeder::class
+	HotelsTableSeeder::class
+	RoomsTableSeeder::class
+	ReservationsTableSeeder::class
+    }
+}
