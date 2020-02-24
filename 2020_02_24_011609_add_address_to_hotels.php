46160ecd662f82375e4e441de3dee37a29bd4b33
jmdupree <59934707+jmdupree@users.noreply.github.com>
2020-02-23 22:11:06 -0500

    Updated hotels schema

---
+++ 2020_02_24_011609_add_address_to_hotels.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddAddressToHotels extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('Address', function (Blueprint $table) {
+	    $table->renameColumn('location', 'Address_1');
+            $table->string('Address_2')->nullable();
+            $table->string('City');
+            $table->string('State');
+	    $table->string('Zip');
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+       
+        Schema::drop('Address');
+    }
+}
