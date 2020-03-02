ca30e330bc3bbd13cadf09dffebe046ccef720d9
jmdupree <59934707+jmdupree@users.noreply.github.com>
2020-03-01 21:31:25 -0500

    Route Reservation

---
+++ web.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function(){
+ return '/ route';
+});
+
+Route::get('/', function(){
+ return '/hotels route';
+});
+
+Route::group([‘prefix’ => ‘reservations’], function(){
+    Route::get('/', function(){
+	return 'Showing users homepage'; });
+    Route::get('reservations', function(){
+	return 'Showing users reservations'; });
+    Route::get('reservations/new', function(){
+	return 'Showing form to create reservations'; });
+    Route::get('reservations/{id}', function($id){
+	return 'Showing reservation id' .$id; });
+    Route::get('reservations/{id}/edit', function($id){
+	return 'Showing edit form for reservation id' .$id; });
+    Route::post('reservations', function(){
+	return 'Creating reservation'; });
+    Route::put('reservations/{id}', function($id){
+	return 'Updating reservation id' .$id });
+    Route::delete('reservations/{id}', function($id){
+	return 'Deleting reservation id' .$id });
+});
